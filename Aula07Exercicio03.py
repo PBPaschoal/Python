2de01d84f24b493871fa9a5e7e8356e08e58097a
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2021-02-17 19:38:08 -0300

    Aula07 Exercicio 03 Curso em Vídeo Python
    Aula07 Exercicio 03 Curso em Vídeo Python
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ Aula07Exercicio03.py
@@ -0,0 +1,5 @@
+#Aula07 Exercicio Curso em Vídeo Python
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+print('Média: {:.1f}'.format((nota1+nota2)/2))
