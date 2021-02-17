fd023d2cca0d30a943633da0866c87efd0b7144e
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2021-02-17 19:37:10 -0300

    Aula07 Exercicio 01 Curso em Vídeo Python
    Aula07 Exercicio Curso em Vídeo Python
    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ Aula07Exercicio01.py
@@ -0,0 +1,4 @@
+#Aula07 Exercicio Curso em Vídeo Python
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+numero = int(input('Informe um número: '))
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(numero, (numero-1), (numero+1)))
