2da0cf813cd18a42f679a6e06b1ba07d8b678944
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2021-02-17 19:39:09 -0300

    Aula07 Exercicio 05 Curso em Vídeo Python
    Aula07 Exercicio 05 Curso em Vídeo Python
    Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada

---
+++ Aula07Exercicio05.py
@@ -0,0 +1,14 @@
+#Aula07 Exercicio Curso em Vídeo Python
+#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+valor = int(input('Informe o valor: '))
+print('{} x  1 = {}'.format(valor, valor*1))
+print('{} x  2 = {}'.format(valor, valor*2))
+print('{} x  3 = {}'.format(valor, valor*3))
+print('{} x  4 = {}'.format(valor, valor*4))
+print('{} x  5 = {}'.format(valor, valor*5))
+print('{} x  6 = {}'.format(valor, valor*6))
+print('{} x  7 = {}'.format(valor, valor*7))
+print('{} x  8 = {}'.format(valor, valor*8))
+print('{} x  9 = {}'.format(valor, valor*9))
+print('{} x 10 = {}'.format(valor, valor*10))
+#Ou print('{} x {:2} = {}'.format(valor, 1, valor*1))
