326e4eb34980bf1257a65f903e631290843b92ad
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2021-02-17 19:38:38 -0300

    Aula07 Exercicio 04 Curso em Vídeo Python
    Aula07 Exercicio Curso em Vídeo Python
    Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.

---
+++ Aula07Exercicio04.py
@@ -0,0 +1,7 @@
+#Aula07 Exercicio Curso em Vídeo Python
+#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+metro = int(input('Informe o valor em metro(s): '))
+cm = metro * 100
+mil = metro * 1000
+print('O valor em centimetros é: {}'.format(cm))
+print('O valor em milimetros é: {}'.format(mil))
