f29d29571e672700671137c18b99818cc2589772
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-02-06 22:31:25 -0300

    Revisando o básico de Python

--- Python 2024/Aleatorio - Praticando/testando.py
+++ Python 2024/Aleatorio - Praticando/testando.py
@@ -18,5 +18,21 @@ else:
     print("O seu número é maior que 10!")
 
 print("Vamos contar até ", numero)
-for i in range(numero):
+for i in range(numero + 1):
     print(i)
+
+idade = int(input("Informe a sua idade: "))
+
+if idade < 18:
+    print("Você é menor de idade e por isso não pode dirigir.")
+else:
+    print("Você já pode dirigir.\n")
+
+digitar = int(input("\nQuantas vezes deseja digitar um número inteiro? "))
+
+soma = 0
+for i in range(digitar):
+    numero_inteiro = int(input("Digite um numero: "))
+    soma += numero_inteiro
+
+print("A soma dos numeros digitados = ", soma)
