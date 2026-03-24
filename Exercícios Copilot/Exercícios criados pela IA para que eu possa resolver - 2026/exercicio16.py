4352bd4a9b5154ba9653d1071b78e0298074eab7
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2026-03-23 21:05:12 -0300

    Exercícios: 016 Converter temperatura de Celsius para Fahrenheit; 017 Calcular área de um círculo; 018 Criar um programa que peça nome e idade e diga se pode votar

---
+++ Exercícios Copilot/Exercícios criados pela IA para que eu possa resolver - 2026/exercicio16.py
@@ -0,0 +1,8 @@
+# Do básico ao avançado!
+# Exercício 16
+# Converter temperatura de Celsius para Fahrenheit
+# - Fórmula: F = C * 9/5 + 32
+# - Dica: peça entrada do usuário.
+celsius_temperature = int(input('Informe a temperatura em Celsius: '))
+fahrenheit = int(celsius_temperature * 9 / 5) + 32
+print(f'Resultado em Fahrenheit: {fahrenheit}ºF')
